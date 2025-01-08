9bdbdf58b1bc7f16ffc67db452c3d76cc0f8e5b8
SHuDiMM <90505584+SHuDiMM@users.noreply.github.com>
2025-01-08 23:12:26 +0700

    module 9.1. 08.01.2025

---
+++ module_9_1.py
@@ -0,0 +1,22 @@
+def apply_all_func(int_list, *functions):
+    for i in int_list:
+        if not isinstance(i, (int, float)):
+            raise TypeError(f"В списке некорректный тип данных: {i} (нужны только числа)")
+
+    for func in functions:
+        if not callable(func):
+            raise TypeError(f"Передан некорректный объект: {func} (нужна функция)")
+
+    results = {}
+    for func in functions:
+        try:
+            results[func.__name__] = func(int_list)
+        except Exception as e:
+            results[func.__name__] = f"Ошибка выполнения: {e}"
+
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
